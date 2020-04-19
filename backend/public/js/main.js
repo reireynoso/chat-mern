30d9dd860890f8534f765b1e6edac457d78535a4
reireynoso <reireynoso@gmail.com>
2020-04-19 13:51:20 -0400

    adding message to the chat

--- backend/public/js/main.js
+++ backend/public/js/main.js
@@ -1,5 +1,39 @@
+const chatForm = document.getElementById("chat-form")
+const chatMessages = document.querySelector('.chat-messages')
 const socket = io();
 
+// message from server
 socket.on('message', message => {
     console.log(message)
+    outputMessage(message)
+
+    // scroll down auto
+    chatMessages.scrollTop = chatMessages.scrollHeight
 })
+
+// Message submit
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get message text
+    const msg = e.target.elements.msg.value
+
+    // emit message to the server
+    socket.emit('chatMessage', msg)
+    
+    // clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus()
+})
+
+// output message to DOM
+
+const outputMessage = (message) => {
+    const div = document.createElement('div')
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+        <p class="text">
+            ${message}
+        </p>`
+    document.getElementsByClassName("chat-messages")[0].append(div)
+}
--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,29 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // run when client connects
 io.on('connection', (socket) => {
-    console.log('new connection')
-    socket.emit('message', 'welcome')
+    
+    // Welcome current user
+    socket.emit('message', 'Welcome to the chat!') // emit just to the user connecting
+
+    socket.broadcast.emit('message', "A user has joined the chat"); //broadcasts to everyone but the connected user
+    
+    // io.emit(); //broadcasts to everyone
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', "A user has left the chat")
+    })
+
+    // listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        console.log(msg)
+        io.emit('message', msg)
+    })
 })
 
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
-      console.log("Running on " + PORT)
+
+    console.log("Running on " + PORT)
 })
