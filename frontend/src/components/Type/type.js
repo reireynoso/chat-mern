ec8559eec9b9dda4968eef0305b8ac4b49429128
reireynoso <reireynoso@gmail.com>
2020-04-23 13:22:54 -0400

    adjusted sentence structure for people typing

--- frontend/src/components/Type/type.js
+++ frontend/src/components/Type/type.js
@@ -2,10 +2,26 @@ import React from 'react'
 import './type.css'
 
 const Type = ({typers}) => {
+    const checkWhich = (index) => {
+        //index passed in accounts for the 0. 1 is added already
+        if(typers.length === 1 || index === typers.length){ 
+            return " "
+        }
+        else if(index + 1 === typers.length){ //compares next number to length
+            return ", and "
+        }
+        else{
+            return ", "
+        }
+    }
+
     return(
         <div className="typers-field">
-            {typers.map(typer => <span>{typer} </span>)}
-            is typing...
+            {typers.map((typer,index) => 
+            <span>{typer}
+                {checkWhich(index + 1)} 
+            </span>)}
+            {typers.length === 1 ? "is" : "are"} typing...
         </div>
     )
 }
